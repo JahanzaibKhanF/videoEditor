f2ef441fb1e709d91d32949046fbbbecaddbcdaa
JahanzaibKhanF <123166354+JahanzaibKhanF@users.noreply.github.com>
2026-07-13 17:50:16 +0500

    Delete src directory

--- src/components/animations/RemotionImageAnimation.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-
-// Dead file - replaced by AnimationEngine.ts canvas rendering
-export const RemotionImageAnimation = () => null;
--- src/components/transitions/RemotionTransitions.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-
-// Dead file - replaced by CompositorCanvas transition rendering
-export const RemotionTransitions = () => null;
